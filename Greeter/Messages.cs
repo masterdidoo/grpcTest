ca5758996bc81b04bdff2b6cc57e06b6f5f00631
Alexandr <975396+masterdidoo@users.noreply.github.com>
2021-11-20 02:36:45 +0400

    init

---
+++ Greeter/Messages.cs
@@ -0,0 +1,18 @@
+﻿using Bond;
+
+namespace RenCapGrpc.Shared
+{
+    [Schema]
+    public class CustomRequest
+    {
+        [Id(0)]
+        public int Payload { get; set; }
+    }
+
+    [Schema]
+    public class CustomResponse
+    {
+        [Id(0)]
+        public int Payload { get; set; }
+    }
+}
